1ef5e0df27086a0b03731ab6edfbe64b3a0d0f4c
Birarvinder Singh Chhabra <birarvindersingh@gmail.com>
2025-06-26 23:24:45 -0600

    Update config.py

--- server/config.py
+++ server/config.py
@@ -1,6 +1,7 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = "sqlite:////home/ubuntu/uptime-arena/server/uptime.db"
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'uptime.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 EMAIL_USER = os.environ.get("EMAIL_USER")
