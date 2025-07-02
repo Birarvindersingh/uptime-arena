c91dfead6951b8eabb591889b989fbbe4110e435
Birarvinder Singh Chhabra <birarvindersingh@gmail.com>
2025-07-01 23:20:26 -0600

    Update config.py

--- server/config.py
+++ server/config.py
@@ -3,7 +3,3 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'uptime.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-EMAIL_USER = os.environ.get("EMAIL_USER")
-EMAIL_PASS = os.environ.get("EMAIL_PASS")
-ALERT_RECIPIENT = os.environ.get("ALERT_RECIPIENT")
